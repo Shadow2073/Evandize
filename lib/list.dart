cfe9b344e87b6f9e8c29bbfbcf8d500ed43e8e7c
Shadow2073 <68701557+Shadow2073@users.noreply.github.com>
2024-02-05 19:15:07 +0530

    add project

---
+++ lib/list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card List'),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text('Card 1'),
+              subtitle: Text('This is card 1'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Card 2'),
+              subtitle: Text('This is card 2'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Card 3'),
+              subtitle: Text('This is card 3'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Card 4'),
+              subtitle: Text('This is card 4'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Card 5'),
+              subtitle: Text('This is card 5'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
